c058e73ffecba57c3397dcb342bc998f35c69f21
Eduardo Souza <superedu2012@gmail.com>
2022-07-21 00:51:10 -0300

    feat:model getClientsById

---
+++ src/model/ativos.js
@@ -0,0 +1,14 @@
+const connection = require('./connection');
+
+const getClientsById = async (id) => {
+    const [result] = await 
+    connection.execute(`
+    SELECT cliente_id, ativo_id, quantidade, valor 
+    FROM DbInvest.carteira as C
+    INNER JOIN DbInvest.ativos as A
+    ON C.ativo_id = A.id
+    where cliente_id = ?;`, [id]);
+    return result;
+};
+
+module.exports = { getClients };
