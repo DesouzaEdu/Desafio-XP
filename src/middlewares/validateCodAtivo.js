fbe83c5c9bace38d22dcdcb282619e32392d4052
Eduardo Souza <superedu2012@gmail.com>
2022-07-24 18:34:33 -0300

    refac: typo

--- src/middlewares/validateCodAtivo.js
+++ src/middlewares/validateCodAtivo.js
@@ -2,9 +2,9 @@ const assets = require('../services/ativos');
 
 const validateCodAtivo = async (req, res, next) => {
     const { codAtivo } = req.body;  
-    if (!codAtivo) return res.status(400).json({ message: '"codCliente" is required' });
+    if (!codAtivo) return res.status(400).json({ message: '"codAtivo" is required' });
     if (typeof codAtivo !== 'number' ) {
-      return res.status(422).json({ message: 'o valor de "codCliente" deve ser númerico' });
+      return res.status(422).json({ message: 'o valor de "codAtivo" deve ser númerico' });
     }
     const assetExists = await assets.getAssetsById(codAtivo);
     if (assetExists.length === 0) return res.status(422).json({ message: '"codAtivo" inválido' });
