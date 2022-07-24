ed6df606caa37ae867bfc085807cca3cebff0693
Eduardo Souza <superedu2012@gmail.com>
2022-07-24 18:19:22 -0300

    change port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./api');
 const routers = require('./router');
 
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (_request, response) => {
   response.send();
