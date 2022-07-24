9828b73d0be3dc4e896bcc1520558c438deec5db
Eduardo Souza <superedu2012@gmail.com>
2022-07-24 18:49:46 -0300

    fix typo

--- src/middlewares/validateValor.js
+++ src/middlewares/validateValor.js
@@ -2,7 +2,7 @@ const validateValor = (req, res, next) => {
     const { valor } = req.body;  
     if (!valor) return res.status(400).json({ message: '"valor" is required' });
     if (valor <= 0 ) {
-      return res.status(422).json({ message: 'o valor não pode ser negativa ou igual a zero' });
+      return res.status(422).json({ message: 'o valor não pode ser negativo ou igual a zero' });
     }
     next();
   };
