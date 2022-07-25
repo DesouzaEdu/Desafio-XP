a23ee59836564853857c052ec7551f4d10fa81b2
Eduardo Souza <superedu2012@gmail.com>
2022-07-24 21:27:04 -0300

    swagger schema investimento

--- src/router.js
+++ src/router.js
@@ -10,8 +10,35 @@ const loginRouter = require('./controllers/login');
 
 const routers = express.Router();
 
+/**
+ * @swagger
+ *  tags:
+ *      name: Investimentos
+ *      description: Endpoints relacionados a compra e venda de investimentos
+ */
 routers.use('/ativos', ativosRouter);
-
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          Investimentos:
+ *              type: object
+ *              required:
+ *                  -codCliente
+ *                  -codAtivo
+ *                  -qtdeAtivo
+ *              properties:
+ *                  codCliente:
+ *                      type: integer
+ *                  codAtivo:
+ *                      type: integer
+ *                  qtdeAtivo:
+ *                      type: integer
+ *              example:
+ *                  codCliente: 2
+ *                  codAtivo: 1
+ *                  qtdeAtivo: 10
+ */
 routers.use('/conta', contaRouter);
 
 routers.use('/investimentos', investimentosRouter);
