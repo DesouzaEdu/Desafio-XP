99418bfa40796f52b3a8afab611f6aba257a9648
Eduardo Souza <superedu2012@gmail.com>
2022-07-24 21:12:11 -0300

    create swagger config

---
+++ src/docs/swagger.config.js
@@ -0,0 +1,18 @@
+const swaggerConfig = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Desafio API-XP",
+            description: "API que simula requisições de uma carteira de investimentos",
+            version: "1.0"
+        },
+        server:[{
+            url: "https://desafio-xp-edu.herokuapp.com/",
+            description: "servidor heroku"
+        }]
+    },
+    apis: ["./src/router.js"]
+    
+}
+
+module.exports = swaggerConfig;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
+const swaggerJsdoc = require('swagger-jsdoc');
 const app = require('./api');
+const swaggerConfig = require('./docs/swagger.config');
 const routers = require('./router');
+const swaggerUi = require('swagger-ui-express');
 
 const port = process.env.PORT || 3000;
+
+const swaggerDoc = swaggerJsdoc(swaggerConfig);
 
 app.get('/', (_request, response) => {
   response.send();
 });
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use(routers);
 app.listen(port, () => console.log('ouvindo porta', port));
